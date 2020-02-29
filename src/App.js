a2306a706b629f9d85df1b9a7f3ab110f4993386
Doriel aboya <doriel494@gmail.com>
2020-02-29 13:59:12 +0200

    log in page update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
 import Logo from './components/Logo';
+import Login from './components/Login';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter as Router , Route , Switch} from 'react-router-dom';
 import './App.css';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       flag:0
+    }
+  }
+  updateFlag=(f)=>{
+    this.setState({flag:f})
+  }
+  showMe=()=>{
+    if (this.state.flag == 1) {
+      return( 
+      <div> 
+        <homepage/>
+      </div>)
+    }
+  }
+  
   render() {
     return (
       <div className="App">
-        
-        
+        <Login flag={this.updateFlag}/>
+        {this.showMe()}
       </div>
     )
   }
