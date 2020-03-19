794c1f4829be073f8cb9b47faa8da9e47ff92cd5
Doriel aboya <doriel494@gmail.com>
2020-03-19 19:34:23 +0200

    arrange the about

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -7,10 +7,10 @@ export default class AboutPage extends Component {
         super(props)
     
         this.state = {
-             About:{Name:"Haim",Location:"hamargalit 47",MyWords:"Barberia was first opened in 1980 by my father Mesfen, the barber shop is filled with history and childhood for me,what made me took care of it for the last 10 years and run it. we specialized in men cuts painting and beard styling."},
-             Criticizes:[{Name:"john doe",date:"12-12-2019",text:"great barber and even better vibe"},
-             {Name:"john doe",date:"12-10-2019",text:"perfect"},
-             {Name:"john doe",date:"01-12-2018",text:"love you guys"},]
+             About:{Name:"Omer dayan",Location:"hamargalit 47",MyWords:"Barberia was first opened in 1980 by my father Mesfen, the barber shop is filled with history and childhood for me,what made me took care of it for the last 10 years and run it. we specialized in men cuts painting and beard styling."},
+             Criticizes:[{id:1,Name:"john doe",date:"12-12-2019",text:"great barber and even better vibe"},
+             {id:2,Name:"john doe",date:"12-10-2019",text:"perfect"},
+             {id:3,Name:"john doe",date:"01-12-2018",text:"love you guys"},]
         }
     }
     
@@ -18,35 +18,32 @@ export default class AboutPage extends Component {
         return (
             <div>
                 <Toolbar/>
-                
-                <div>
-                <div className="card mb-3" style={{maxWidth: 540}}>
-                    <div className="row no-gutters">
+                    <div style={{backgroundColor:"white"}} className="row no-gutters">
                       <div className="col-md-4">
-                        <img style={{width:90, borderRadius:50}} src="/od.jpg" class="card-img" alt="..."/>
+                          <div className="container">
+                          <div className="row">
+                              <div className="col-6">
+                              <img style={{width:90, borderRadius:50, marginTop:20}} src="/od.jpg" className="card-img" alt="..."/>
+                              </div>
+                              <div style={{textAlign:"left"}} className="col-6"><h3 className="nameTitle">{this.state.About.Name}</h3></div>
+                          </div>
                       </div>
                       <div className="col-md-8">
                         <div className="card-body">
-                        <h3>{this.state.About.Name}</h3>
                         <span style={{fontWeight:"bold"}}>Address: </span>{this.state.About.Location}
-                          <h5 className="card-title">My Space</h5>
                           <p className="card-text">{this.state.About.MyWords}</p>
                         </div>
                       </div>
                     </div>
                   </div>
-                  </div>
-
-                {this.state.Criticizes.map((element)=>{
-                    return <div style={{height:100}} className="criticize">
+                    <div className="container con" >
+                {this.state.Criticizes.map((element, key)=>{
+                    return <div key={element.id} className="criticize">
                         <span className="criticName">{element.Name}</span> <br/>
                         {element.text}<br/> <p className="dateOfCritic">{element.date} </p>
                     </div>
-                    
                 })}
-
-
-
+                </div>
                 <div className="AboutLogo"><Logo variant="little"/></div>
             </div>
         )
