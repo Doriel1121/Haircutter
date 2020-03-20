006d20ace07c6ec8bb45e4a0a4287c48ff3c3d80
Doriel aboya <doriel494@gmail.com>
2020-03-20 14:23:34 +0200

    mobile satate added

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import ReservationPage from './components/ReservationPage';
 import AboutPage from './components/AboutPage'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter as Router , Route , Switch} from 'react-router-dom';
+import {isMobile} from 'react-device-detect';
+
 
 export default class App extends Component {
   constructor(props) {
@@ -23,31 +25,34 @@ export default class App extends Component {
   }
 
   render() {
-    return (
-      <div className="App">
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <LoginPage/>
-          </Route>
-          <Route exact path = "/register">
-            <RegisterPage nameOfCritisizer={this.updateTheName}/>
-          </Route>
-          <Route exact path ="/homepage">
-            <HomePage/>
-          </Route>
-          <Route exact path ="/history">
-            <HistoryPage/>
-          </Route>
-          <Route exact path="/reservation">
-            <ReservationPage/>
-          </Route>
-          <Route exact path ="/about">
-            <AboutPage name={this.state.Name}/>
-          </Route>
-        </Switch>
-      </Router>
-      </div>
-    )
+    if (isMobile) {
+      return (
+        <div className="App">
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <LoginPage/>
+            </Route>
+            <Route exact path = "/register">
+              <RegisterPage nameOfCritisizer={this.updateTheName}/>
+            </Route>
+            <Route exact path ="/homepage">
+              <HomePage/>
+            </Route>
+            <Route exact path ="/history">
+              <HistoryPage/>
+            </Route>
+            <Route exact path="/reservation">
+              <ReservationPage/>
+            </Route>
+            <Route exact path ="/about">
+              <AboutPage name={this.state.Name}/>
+            </Route>
+          </Switch>
+        </Router>
+        </div>
+      )
+    }else{return <div className="App">Please change to mobile state </div>}
+    
   }
 }
